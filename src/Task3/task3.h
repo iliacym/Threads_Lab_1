d4bbad1fbdb72d99ec958edfbf613b79cc2eeb26
boris <boriskozinn@gmail.com>
2024-10-15 10:38:35 +0300

    template for task3

--- src/Task3/task3.h
+++ src/Task3/task3.h
@@ -1,4 +1,13 @@
 #ifndef TASK3_H
 #define TASK3_H
+#include "pthread.h"
+
+typedef struct TASK3_rwlock_t {
+    pthread_mutex_t mutex;
+    pthread_cond_t write_cond;
+    pthread_cond_t read_cond;
+    unsigned long long int write_wait, read_wait;
+    int is_locked;
+} TASK3_rwlock_t;
 
 #endif
