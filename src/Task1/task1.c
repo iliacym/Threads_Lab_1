726e1fb8f928db6fd533f5334ec165245c0439ef
boris <boriskozinn@gmail.com>
2024-10-22 22:22:51 +0300

    add readme.md

--- src/Task1/task1.c
+++ src/Task1/task1.c
@@ -9,8 +9,8 @@ typedef struct {
     unsigned long long int trials;
 } thread_data_t;
 
-void *monte_carlo_pi(void *args) {
-    thread_data_t *data = (thread_data_t *) args;
+void* monte_carlo_pi(void *args) {
+    thread_data_t const *data = (thread_data_t*)args;
     unsigned long long int hits = 0;
 
     unsigned int seed = time(NULL) ^ pthread_self();
@@ -30,17 +30,20 @@ void *monte_carlo_pi(void *args) {
     return NULL;
 }
 
-double TASK1_run(unsigned long long int ntrials, int nthreads) {
+double TASK1_run(unsigned long long int const ntrials, int const nthreads) {
     pthread_t threads[nthreads];
     thread_data_t thread_data[nthreads];
-    unsigned long long int trials_per_thread = ntrials / nthreads;
+    unsigned long long int const trials_per_thread = ntrials / nthreads;
     total_hits = 0;
     pthread_mutex_init(&mutex, NULL);
 
-    for (int i = 0; i < nthreads; i++) {
+    for (int i = 0; i < nthreads - 1; i++) {
         thread_data[i].trials = trials_per_thread;
         pthread_create(&threads[i], NULL, monte_carlo_pi, &thread_data[i]);
     }
+
+    thread_data[nthreads - 1].trials = trials_per_thread + ntrials % nthreads;
+    pthread_create(&threads[nthreads - 1], NULL, monte_carlo_pi, &thread_data[nthreads - 1]);
 
     for (int i = 0; i < nthreads; i++) {
         pthread_join(threads[i], NULL);
@@ -48,5 +51,5 @@ double TASK1_run(unsigned long long int ntrials, int nthreads) {
 
     pthread_mutex_destroy(&mutex);
 
-    return 4.0 * (double) total_hits / (double) ntrials;
+    return 4.0 * (double)total_hits / (double)ntrials;
 }
